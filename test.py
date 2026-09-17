54f5a9baf22dd854d472cf5e3b338cfd15c2c54d
duckies1 <adianand1729@gmail.com>
2026-09-17 12:48:41 +0530

    test run of yolo8

---
+++ test.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+# model = YOLO.from_pretrained("Ultralytics/YOLO26")
+model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
+source = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+model.predict(source=source, save=True)
